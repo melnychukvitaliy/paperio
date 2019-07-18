ea8b899b6fe6cdac186fdeb6aa7285cefd7db959
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2019-07-19 02:36:33 +0300

    add process tick function

--- bot.py
+++ bot.py
@@ -4,8 +4,28 @@ Paperio bot implementation.
 import json
 import random
 
+UP = 'up'
+LEFT = 'left'
+RIGHT = 'right'
+DOWN = 'down'
+
+
+def process_tick(state):
+    '''
+    Define direction for each tick
+    '''
+    commands = [LEFT, RIGHT, UP, DOWN]
+    cmd = random.choice(commands)
+    return {"command": cmd, 'debug': json.dumps(state['params'])}
+
+
 while True:
-    STATE = input()
-    COMMANDS = ['left', 'right', 'up', 'down']
-    CMD = random.choice(COMMANDS)
-    print(json.dumps({"command": CMD, 'debug': str(STATE)}))
+    STATE = json.loads(input())
+
+    print(json.dumps({"command": UP, 'debug': STATE['type']}))
+
+    if STATE['type'] == 'start_game':
+        print(json.dumps({"command": "up"}))
+
+    if STATE['type'] == 'tick':
+        print(json.dumps(process_tick(STATE)))
